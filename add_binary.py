5122eb60970eda8a836fce50d816546f13d3a55a
Bulat <askarovroman181@gmail.com>
2024-11-23 20:45:22 +0300

    fixed bugs

--- add_binary.py
+++ add_binary.py
@@ -1,12 +1,4 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-
         res = bin(int(a, 2) + int(b, 2))
         return res[2:]
-
-
-solution = Solution()
-a = "11"
-b = "1"
-x = solution.addBinary(a, b)
-print(x)
