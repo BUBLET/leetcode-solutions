ceafd2716d4d27e014d914365c1ade1f5767249c
Bulat <askarovroman181@gmail.com>
2024-12-01 00:39:39 +0300

    max_profit.py update

--- max_profit.py
+++ max_profit.py
@@ -2,17 +2,34 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        while prices:
-            min_val = min(prices)
-            idx = prices.index(min_val)
-            max_val = max(prices[idx:])
-            count = 
+        
+        profit = 0
+        min_price = prices[0]
+
+        for i in range(len(prices)):
+
+            # Если текущая цена наименьшая - работаем с ней
+            if prices[i] < min_price:
+                min_price = prices[i]
+                continue
+            
+            profit_curr = prices[i] - min_price
+            if profit_curr > profit:
+                profit = profit_curr
+
+
+                
+        return profit
+            
+
+
 
         
 
 
 prices = [7,1,5,3,6,4]
-solutor = Solution
+solutor = Solution()
+
 x = solutor.maxProfit(prices)
 print(x)
 
